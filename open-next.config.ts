e55a0ba488294a7d17e6c8dc19a0f3d6b1660797
OneNok_HK <honokhei20061122@gmail.com>
2026-02-21 23:55:25 +0800

    fix: correct open-next config format

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,8 +1,11 @@
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
-
-const config: OpenNextConfig = {
+const config = {
   default: {
-  }
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
 };
 
 export default config;
