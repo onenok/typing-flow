962f6900206961e2c111671a0b9b6eca16e39024
OneNok_HK <honokhei20061122@gmail.com>
2026-04-06 12:52:20 +0800

    feat: added src/app/contact/page.tsx

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Link from "next/link";
+
+export default function ContactPage() {
+  return (
+    <div className="h-full bg-linear-to-b from-blue-50 to-indigo-100">
+      <main className="container mx-auto px-4 py-8">
+        <h1 className="
+        text-5xl font-bold text-gray-900 mb-6 text-center
+        ">
+          聯繫我們
+        </h1>
+
+        <div className="max-w-4xl mx-auto">
+          <div className="
+            bg-white rounded-lg shadow-md
+            p-6 mb-8
+            text-gray-700
+          ">
+
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
